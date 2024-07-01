ba2c6cc992557d2dc043168295623e973a028bb4
wuiquique <luisenriquem15@gmail.com>
2024-06-30 22:29:11 -0600

    Spotify Clone Grid and Sidebar

---
+++ tailwind.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        "colorbg-card": "rgb(19, 19, 19)",
+        "colorbg-card-hover": "rgb(25, 25, 25)",
+        "color-basic-text": "rgb(170, 170, 170)",
+        "colorbg-folder": "rgb(36, 36, 36)",
+      },
+    },
+  },
+  plugins: [],
+};
